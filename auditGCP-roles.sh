acf48bc1be7bf230f740bed933c3339e8bcd7b99
hamza-gheggad <41976870+hamza-gheggad@users.noreply.github.com>
2021-01-19 11:10:28 +0100

    Update auditGCP-roles.sh

--- auditGCP-roles.sh
+++ auditGCP-roles.sh
@@ -1,14 +1,26 @@
 #!/bin/bash
-ROLE="roles/owner"
 for PROJECT in $(\
   gcloud projects list \
   --format="value(projectId)" \
   )
 do
-  printf "%s:\n" ${PROJECT}
-  gcloud projects get-iam-policy ${PROJECT} \
-  --flatten="bindings[].members[]" \
-  --filter="bindings.role=${ROLE}" \
-  --format="value(bindings.members)"
-  printf "\n"
+    printf "%s:\n" ${PROJECT}
+    Users=$(gcloud projects get-iam-policy ${PROJECT} | grep user\: | cut -d':' -f 2)
+    set -- $Users
+    while [ $# -gt 0 ]
+    do
+        User="$1"
+        if [ -z "$User" ]
+        then
+            :
+        else
+            echo $User
+            gcloud projects get-iam-policy ${PROJECT} \
+            --flatten="bindings[].members" \
+            --format="table(bindings.role)" \
+            --filter="bindings.members:$User"
+            printf "\n"
+        fi
+        shift
+    done  
 done
